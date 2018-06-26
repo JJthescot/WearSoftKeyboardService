1e851edbe6026bcdebabb9845ca969e63bcb8680
John McBeath <john_mcbeath_5@hotmail.com>
2018-06-26 14:15:33 +0100

    Basic keyboard implementation completed.

--- app/src/main/java/com/homeapps/john/wear_soft_keyboard_service/SimpleKeyboardIME.java
+++ app/src/main/java/com/homeapps/john/wear_soft_keyboard_service/SimpleKeyboardIME.java
@@ -1,16 +1,93 @@
 package com.homeapps.john.wear_soft_keyboard_service;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
-public class SimpleKeyboardIME extends Service {
-    public SimpleKeyboardIME() {
+public class SimpleKeyboardIME extends InputMethodService implements OnKeyboardActionListener {
+
+    private KeyboardView keyboardView;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+        LinearLayout myView = (LinearLayout) View.inflate(this, R.layout.keyboard_layout, null);
+        ScrollView scrollView = (ScrollView) myView.findViewById(R.id.keyboard_scroll);
+        keyboardView = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+
+        keyboard = new Keyboard(this, R.xml.alphabetty);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+
+        scrollView.addView(keyboardView);
+        return myView;
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                keyboardView.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && caps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
     }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+
 }
